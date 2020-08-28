0c329cac721b8bbd43ef14cc6c0b88b5f1533c03
Reuben Schiopu <59889074+MangoCoder360@users.noreply.github.com>
2020-08-27 17:02:35 -0700

    Update blockchain.py

--- blockchain.py
+++ blockchain.py
@@ -1,14 +1,21 @@
 class Blockchain(object):
 
 	self.new_block(previous_hash=1, proof=100)
-	
+
 	def __init__(self):
 		self.chain = []
 		self.current_transactions = []
 
 	def new_block(self, proof, previous_hash=None):
-		#This function creates new blocks and then adds to the existing chain
-		pass
+		block = {
+		‘index’: len(self.chain) + 1,
+		‘timestamp’ : time(),
+		‘proof’: proof,
+		previous_hash: previous_hash or self.hash(self.chain[-1]),
+	}
+	self.current_transactions=[]
+	self.chain.append(block)
+	return block
 
 	def new_transaction(self):
 		self.current_transactions.append(
