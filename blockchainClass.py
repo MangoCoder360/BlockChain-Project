2f476107bd4f42a3b0419d19006711e8bb6cff71
Reuben Schiopu <59889074+MangoCoder360@users.noreply.github.com>
2020-08-28 18:11:35 -0700

    Reorganized files

--- blockchainClass.py
+++ blockchainClass.py
@@ -5,6 +5,18 @@ class Blockchain(object):
 		self.current_transactions = []
 		self.new_block(previous_hash=1, proof=100)
 
+	def proof_of_work(self, last_proof):
+                proof = 0
+                while self.valid_proof(last_proof, proof) is False:
+                        proof +=1
+                return proof
+
+        @staticmethod
+        def valid_proof(last_proof, proof):
+                guess = f'{last_proof}{proof}'.encode()
+                guess_hash = hashlib.sha256(guess).hexigest()
+                return guess_hash[:4] == “000”
+
 	def new_block(self, proof, previous_hash=None):
 		block = {
 		'index': len(self.chain) + 1,
